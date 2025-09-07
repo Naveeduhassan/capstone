67fdee3a7f1af8d7dc210c7beeeab047629e7aa4
Naveeduhassan <naveedulhassan048@gmail.com>
2025-09-07 14:41:49 -0500

    Make About Section and Footer Section with modern UI

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      
-    </div>
+    <section className="about">
+      <div className="about-Content">
+      <div className="about-header">
+        <h2 className="about-title">Little Lemon</h2>
+        <p>Chicago</p>
+      </div>
+      <p className="about-text">
+        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
+      </p>
+      </div>
+      <div className="about-image">
+        <img src="https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg" alt="About Little Lemon 1" className="about-image-1" />
+        <img src="https://www.themealdb.com/images/media/meals/qwxuwy1511798027.jpg" alt="About Little Lemon 2" className="about-image-2" />
+      </div>
+    </section>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,43 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer Content</h1>
-    </div>
+    <footer className="footer">
+      <div className="footer-image">
+        <img
+          src="https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg"
+          alt="Little Lemon Logo"
+        />
+      </div>
+      <div className="footer-columns">
+        <div className="footer-column">
+          <h4>Doormat Navigation</h4>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h4>Contact</h4>
+          <ul>
+            <li>Address</li>
+            <li>Phone number</li>
+            <li>Email</li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h4>Social Media Links</h4>
+          <ul>
+            <li>Address</li>
+            <li>Phone number</li>
+            <li>Email</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   )
 }
 
